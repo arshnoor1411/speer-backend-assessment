32c8f9951f979c1d8d40573946c50be13a071999
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-03 18:45:37 +0530

    enhance: required modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { applicationConfig } from 'config/application-config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string(),
+        DATABASE_POST: Joi.string(),
+        DATABASE_NAME: Joi.string(),
+        DATABASE_USER: Joi.string(),
+        DATABASE_PASSWORD: Joi.string(),
+        PORT: Joi.number().default(5050),
+        ENV: Joi.string()
+          .valid('development', 'base', 'beta', 'qa')
+          .default('development'),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: applicationConfig.db.host,
+      port: parseInt(applicationConfig.db.port),
+      username: applicationConfig.db.user,
+      password: applicationConfig.db.password,
+      database: applicationConfig.db.name,
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
