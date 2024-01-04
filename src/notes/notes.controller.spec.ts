fc86f8f66e61312d2ddc0703e417ebe722a81a58
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 17:45:27 +0530

    enhance: notes test case added

--- src/notes/notes.controller.spec.ts
+++ src/notes/notes.controller.spec.ts
@@ -1,12 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
+import Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { applicationConfig } from 'config/application-config';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { Note } from './entities/note.entity';
+import { NotesModule } from './notes.module';
 
 describe('NotesController', () => {
   let controller: NotesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: Joi.object({
+            DATABASE_HOST: Joi.string(),
+            DATABASE_POST: Joi.string(),
+            DATABASE_NAME: Joi.string(),
+            DATABASE_USER: Joi.string(),
+            DATABASE_PASSWORD: Joi.string(),
+            PORT: Joi.number().default(5050),
+            ENV: Joi.string()
+              .valid('development', 'base', 'beta', 'qa')
+              .default('development'),
+          }),
+        }),
+
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: applicationConfig.db.host,
+          port: parseInt(applicationConfig.db.port),
+          username: applicationConfig.db.user,
+          password: applicationConfig.db.password,
+          database: applicationConfig.db.name,
+          entities: [User, Note],
+          synchronize: true,
+        }),
+
+        JwtModule.register({
+          secret: applicationConfig.jwt.secret,
+          signOptions: { expiresIn: '12h' },
+        }),
+
+        ThrottlerModule.forRoot([
+          {
+            ttl: applicationConfig.rateLimit.ttl,
+            limit: applicationConfig.rateLimit.limit,
+          },
+        ]),
+
+        UsersModule,
+        JwtModule,
+        NotesModule,
+      ],
       controllers: [NotesController],
       providers: [NotesService],
     }).compile();
--- src/notes/notes.service.spec.ts
+++ src/notes/notes.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesService } from './notes.service';
+import { Note } from './entities/note.entity';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { applicationConfig } from '../../config/application-config';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { UsersModule } from '../users/users.module';
+import { NotesModule } from './notes.module';
+import { User } from '../users/entities/user.entity';
 
 describe('NotesService', () => {
-  let service: NotesService;
+  let notesService: NotesService;
+  let notesRepository: Repository<Note>;
+  const NOTE_REPOSITORY_TOKEN = getRepositoryToken(Note);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
+      providers: [
+        NotesService,
+        {
+          provide: NOTE_REPOSITORY_TOKEN,
+          useValue: { create: jest.fn(), save: jest.fn(), findOne: jest.fn() },
+        },
+      ],
     }).compile();
 
-    service = module.get<NotesService>(NotesService);
+    notesService = module.get<NotesService>(NotesService);
+    notesRepository = module.get<Repository<Note>>(getRepositoryToken(Note));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(notesService).toBeDefined();
+  });
+
+  it('userRepository should be defined', () => {
+    expect(notesService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('userRepository should be defined', async () => {
+      await notesService.create(
+        { data: 'Hello World Lorem Ipsum' },
+        '80836db6-5d2a-4a9c-965c-11a4606b873c',
+      );
+    });
   });
 });
