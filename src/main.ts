3b4663247e825127e62a24a8a16c385e5151cbae
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 16:30:16 +0530

    enhance: swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(3000, '0.0.0.0');
+
+  const config = new DocumentBuilder()
+    .setTitle('Speer Backend')
+    .setDescription('Speer Backend APIs')
+    .setVersion('1.0')
+    .addTag('speer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
