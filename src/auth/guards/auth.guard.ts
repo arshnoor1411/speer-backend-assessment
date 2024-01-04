7079d16d3fb8e5b042b81b81ff30293dca16beed
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 16:13:23 +0530

    enhance: auth guard added to server

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,56 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { applicationConfig } from 'config/application-config';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractJwtToken(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: applicationConfig.jwt.secret,
+      });
+
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request.user = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractJwtToken(request: any): string | null {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return null;
+    }
+
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer.toLowerCase() !== 'bearer' || !token) {
+      return null;
+    }
+
+    return token;
+  }
+}
