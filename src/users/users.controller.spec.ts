a0b4200d38a223e54ebb4b33d04a5d9102273ade
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 17:45:43 +0530

    enhance: user test file added

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { applicationConfig } from '../../config/application-config';
+import { Note } from '../notes/entities/note.entity';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { UsersModule } from './users.module';
+import { NotesModule } from '../notes/notes.module';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: Joi.object({
+            DATABASE_HOST: Joi.string(),
+            DATABASE_POST: Joi.string(),
+            DATABASE_NAME: Joi.string(),
+            DATABASE_USER: Joi.string(),
+            DATABASE_PASSWORD: Joi.string(),
+            PORT: Joi.number().default(5050),
+            ENV: Joi.string()
+              .valid('development', 'base', 'beta', 'qa')
+              .default('development'),
+          }),
+        }),
+
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: applicationConfig.db.host,
+          port: parseInt(applicationConfig.db.port),
+          username: applicationConfig.db.user,
+          password: applicationConfig.db.password,
+          database: applicationConfig.db.name,
+          entities: [User, Note],
+          synchronize: true,
+        }),
+
+        ThrottlerModule.forRoot([
+          {
+            ttl: applicationConfig.rateLimit.ttl,
+            limit: applicationConfig.rateLimit.limit,
+          },
+        ]),
+
+        UsersModule,
+        JwtModule,
+        NotesModule,
+        User,
+        Note,
+      ],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { Note } from '../notes/entities/note.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  let userRepository: Repository<User>;
+
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(Note);
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: { create: jest.fn(), save: jest.fn(), findOne: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('userRepository should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('userRepository should be defined', async () => {
+      await service.create({
+        firstName: 'Arshnoor',
+        lastName: 'Singh',
+        email: 'arshnoors196@gmail.com',
+        password: 'test@123',
+        otp: '123456',
+      });
+    });
   });
 });
