8005179d5069abf50562029a3fa73d362a89c73b
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 13:20:12 +0530

    delete: removed unused modules

--- src/sendgrid/sendgrid.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SendgridService } from './sendgrid.service';
-import { SendgridController } from './sendgrid.controller';
-
-@Module({
-  controllers: [SendgridController],
-  providers: [SendgridService],
-})
-export class SendgridModule {}
