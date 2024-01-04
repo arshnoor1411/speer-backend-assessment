3749298332cf82f038e5d2426111d072923d7a53
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-04 16:12:12 +0530

    enhance: migration for note added

---
+++ migrations/1704355045326-notes-entity.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class NotesEntity1704355045326 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'notes',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'created_by',
+            isNullable: false,
+            type: 'varchar',
+          },
+          {
+            name: 'data',
+            isNullable: false,
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
