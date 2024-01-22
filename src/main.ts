f1182f5b01bd02a06451ecf9d88acf0e79271b65
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-22 15:20:27 +0530

    enhance: added enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: ['http://localhost:3000'] });
   await app.listen(4000, '0.0.0.0');
 
   const config = new DocumentBuilder()
